fa64950f8d73bfece9ba8e1ff57bbb8bf8cfb53e
npkg2pkc-ux <npkg2.pkc@gmail.com>
2025-11-21 06:54:58 +0700

    Update fitur xyz

--- src/App.tsx
+++ src/App.tsx
@@ -4138,7 +4138,7 @@ export default function ProduksiNPKApp() {
                 <BarChart
                   data={calculateWRPerEksekutor(wrChartPeriod, wrChartMonth)}
                   layout="vertical"
-                  margin={{ top: 10, right: 40, left: 150, bottom: 10 }}
+                  margin={{ top: 10, right: 40, left: 100, bottom: 10 }}
                 >
                   <defs>
                     <linearGradient id="wrGradient" x1="0" y1="0" x2="1" y2="0">
