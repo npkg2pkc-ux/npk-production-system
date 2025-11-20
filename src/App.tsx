4892ef580f63a03065fa8a7fa13c1a8494abbc87
npkg2pkc-ux <npkg2.pkc@gmail.com>
2025-11-20 21:08:59 +0700

    Update fitur xyz

--- src/App.tsx
+++ src/App.tsx
@@ -8122,7 +8122,7 @@ export default function ProduksiNPKApp() {
               v1.18 - 2025 | NPKG-2 Production
             </p>
             <p className="text-xs opacity-75 mt-1">
-              Made with <span className="text-red-500">❤️</span>
+              Made with <span className="text-red-500">🤍</span>
             </p>
           </div>
         </div>
